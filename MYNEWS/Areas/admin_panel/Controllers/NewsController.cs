927c9196c566032c461491e29008f4a87b87c124
Zeynal Zeynalov <zeynal.zek.2001@gmail.com>
2024-04-22 18:18:55 +0400

    The view page of the admin panel is partially completed

---
+++ MYNEWS/Areas/admin_panel/Controllers/NewsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MYNEWS.Enums;
+
+namespace MYNEWS.Areas.admin_panel.Controllers
+{
+    [Area("admin_panel")]
+    [Authorize(Roles = nameof(RoleTypes.Admin), AuthenticationSchemes = "AdminAuth")]
+    public class NewsController : Controller
+    {
+        public IActionResult GetAllNews()
+        {
+            return View();
+        }
+
+        public IActionResult GetDeletedNews()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ MYNEWS/Areas/admin_panel/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MYNEWS.Enums;
+
+namespace MYNEWS.Areas.admin_panel.Controllers
+{
+    [Area("admin_panel")]
+    [Authorize(Roles = nameof(RoleTypes.Admin), AuthenticationSchemes = "AdminAuth")]
+    public class UsersController : Controller
+    {
+        public IActionResult GetAllUsers()
+        {
+            return View();
+        }
+        
+        public IActionResult GetDeletedUsers()
+        {
+            return View();
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
